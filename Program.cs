65a1c3fc03bc9a929b879a7a6c7698f7c033913d
amigo500 <alexhouser@hotmail.com>
2022-04-09 20:55:44 +0200

    add machines

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
-using A17MMS;
-using Microsoft.AspNetCore.Components.Web;
+using A17MMS.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+namespace A17MMS
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-await builder.Build().RunAsync();
+            builder.Services.AddHttpClient<IMachineDataService, MachineDataService>(client =>
+                //client.BaseAddress = new Uri("http://localhost:7071/")); //Local
+                client.BaseAddress = new Uri("https://msfuncapi.azurewebsites.net/")); //Production
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
